bdbb9744ec7803e5203a4520a9ca517cc41e88a4
Mateus-denv <187669882+Mateus-denv@users.noreply.github.com>
2026-04-01 19:27:46 -0300

    Construído a classe para que o banco de dados entenda a relação.

--- src/main/java/br/com/campushop/campushop_backend/model/Categoria.java
+++ src/main/java/br/com/campushop/campushop_backend/model/Categoria.java
@@ -3,15 +3,26 @@ package br.com.campushop.campushop_backend.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "categoria") // Certifique-se que o nome na tabela é este
+@Table(name = "categoria") // Especifica o nome da tabela no banco de dados, que é "categoria"
 public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_categoria")
     private Integer idCategoria;
     
+@Column(name = "nome_categoria", nullable = false, length = 100)
     private String nome_categoria;
+
+    @Column(columnDefinition = "TEXT")
     private String descricao;
 
-    // Getters e Setters
+    // --- GETTERS E SETTERS ---
+    public Integer getIdCategoria() { return idCategoria; }
+    public void setIdCategoria(Integer idCategoria) { this.idCategoria = idCategoria; } // O ID é gerado automaticamente, então geralmente não precisamos de um setter para ele
+
+    public String getNomeCategoria() { return nome_categoria; }
+    public void setNomeCategoria(String nomeCategoria) { this.nome_categoria = nomeCategoria; } // O nome da categoria é obrigatório, então não pode ser nulo
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; } // A descrição é opcional, então pode ser nula
 }
